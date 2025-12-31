79749f84e9fa3f1a363a8acf6e8d5b8cf3ddd8db
Fatemehsabaghjafari <113588373+Fatemehsabaghjafari@users.noreply.github.com>
2025-12-31 12:00:06 +0100

    reports router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.care_sessions.router import router as care_sessions_router
+from app.reports.router import router as reports_router
 from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
 
 app.include_router(care_sessions_router)
+app.include_router(reports_router)
 
 @app.get("/health")
 def health():
